2f23a2b6c8afe993922e35b0f22b29e775cde132
Yebin46 <binwoo1222@ewhain.net>
2021-08-26 07:11:49 +0000

    add multi model tester

--- test_models.py
+++ test_models.py
@@ -3,17 +3,17 @@ import pandas as pd
 import numpy as np
 import argparse
 from PIL import Image
-import tqdm
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 
-# from torchvision import transforms
-# from torchvision.transforms import Resize, ToTensor, Normalize
+import torchvision
 from albumentations import *
 from albumentations.pytorch import ToTensorV2
+
+import tqdm
 
 # 테스트 데이터셋 폴더 경로를 지정해주세요.
 test_dir = '/opt/ml/input/data/eval'
@@ -68,22 +68,43 @@ if __name__ == '__main__':
         shuffle=False
     )
 
-    # 모델을 정의합니다. (학습한 모델이 있다면 torch.load로 모델을 불러주세요!)
+
     device = torch.device('cuda')
     
-    # model_best_checkpoint = glob.glob('/opt/ml/checkpoint/{}_{}_*.pt'.format(args.model_name, args.epoch_num))[0]
-    model_best_checkpoint = glob.glob('/opt/ml/checkpoint/{}*.pt'.format(args.model_path))[0]
-    print('model checkpoint: ', model_best_checkpoint)
-    model = torch.load(model_best_checkpoint)
-    model.eval()
+    age_model = torchvision.models.resnet18(pretrained=False)
+    mask_model = torchvision.models.resnet18(pretrained=False)
+    gender_model = torchvision.models.resnet18(pretrained=False)
+    age_model.fc = torch.nn.Linear(in_features=512, out_features=3, bias=True)
+    mask_model.fc = torch.nn.Linear(in_features=512, out_features=3, bias=True)
+    gender_model.fc = torch.nn.Linear(in_features=512, out_features=2, bias=True)
+    
+    checkpoint_path = glob.glob('/opt/ml/checkpoint/{}*.pt'.format(args.model_path))[0]
+    print('model checkpoint: ', checkpoint_path)
+    
+    checkpoint = torch.load(checkpoint_path)
+    age_model.load_state_dict(checkpoint['age_model_state_dict'])
+    mask_model.load_state_dict(checkpoint['mask_model_state_dict'])
+    gender_model.load_state_dict(checkpoint['gender_model_state_dict'])
+
+    age_model.eval()
+    mask_model.eval()
+    gender_model.eval()
+    
+    age_model = age_model.to(device)
+    mask_model = mask_model.to(device)
+    gender_model = gender_model.to(device)
 
     # 모델이 테스트 데이터셋을 예측하고 결과를 저장합니다.
     all_predictions = []
     for images in tqdm.tqdm(loader):
         with torch.no_grad():
             images = images.to(device)
-            pred = model(images)
-            pred = pred.argmax(dim=-1)
+            
+            age = age_model(images).argmax(dim=-1)
+            mask = mask_model(images).argmax(dim=-1)
+            gender = gender_model(images).argmax(dim=-1)
+            
+            pred = age + 6*mask + 3*gender
             all_predictions.extend(pred.cpu().numpy())
     submission['ans'] = all_predictions
 
--- train_models.py
+++ train_models.py
@@ -10,6 +10,7 @@ from models.model import get_model
 import argparse
 from pytz import timezone
 import datetime as dt
+from sklearn.metrics import f1_score
 
 def train_models(args):
     now = (dt.datetime.now().astimezone(timezone("Asia/Seoul")).strftime("%m%d_%H%M"))
@@ -27,8 +28,23 @@ def train_models(args):
     #     model = torchvision.models.vgg16(pretrained=False)
     #     model.classifier[6] = nn.Linear(in_features=4096, out_features=18, bias=True)
     
+    if args.torchvision:
+        age_model = torchvision.models.resnet18(pretrained=False)
+        mask_model = torchvision.models.resnet18(pretrained=False)
+        gender_model = torchvision.models.resnet18(pretrained=False)
+        age_model.fc = torch.nn.Linear(in_features=512, out_features=3, bias=True)
+        mask_model.fc = torch.nn.Linear(in_features=512, out_features=3, bias=True)
+        gender_model.fc = torch.nn.Linear(in_features=512, out_features=2, bias=True)
+        if args.pretrained:
+            checkpoint = torch.load(args.pretrained)
+            age_model.load_state_dict(checkpoint['age_model_state_dict'])
+            mask_model.load_state_dict(checkpoint['mask_model_state_dict'])
+            gender_model.load_state_dict(checkpoint['gender_model_state_dict'])
     # Custom models
-    if args.pretrained:
+    elif args.pretrained:
+        '''
+        have to change
+        '''
         age_model = torch.load(args.pretrained_age)
         gender_model = torch.load(args.pretrained_gender)
         mask_model = torch.load(args.pretrained_mask)
@@ -51,11 +67,12 @@ def train_models(args):
     gender_model = gender_model.to(device)
     mask_model = mask_model.to(device)
 
-    max_val_acc = 0
+    max_val_f1 = 0
     age_model.train()
     gender_model.train()
     mask_model.train()
     for epoch in range(args.epochs):
+        f1 = 0
         total_acc = 0
         total_age_acc = 0
         total_mask_acc = 0
@@ -90,6 +107,7 @@ def train_models(args):
             # make real class
             pred = torch.max(age_pred, 1)[1] + 3*torch.max(gender_pred, 1)[1] + 6*torch.max(mask_pred, 1)[1]
             acc = torch.sum(pred == label)/len(label)
+            f1 += f1_score(label.cpu().numpy(), pred.cpu().numpy(), average='macro')
             
             print('[epoch: {}, iter: {}] Age train acc: {:.5f}, Gender train acc: {:.5f}, Mask train acc: {:.5f}, train acc: {:.5f}'.format(epoch, batch_iter, age_acc, gender_acc, mask_acc, acc))
             
@@ -120,12 +138,13 @@ def train_models(args):
         avg_mask_loss = total_mask_loss/len(train_dataloader)
         avg_gender_loss = total_gender_loss/len(train_dataloader)
         
+        f1 = f1/(batch_iter+1)
         avg_acc = total_acc/len(train_dataloader)
         avg_age_acc = total_age_acc/len(train_dataloader)
         avg_mask_acc = total_mask_acc/len(train_dataloader)
         avg_gender_acc = total_gender_acc/len(train_dataloader)
         
-        print('[epoch: {}] avg train acc: {:.5f}, avg Age acc: {:.5f}, avg Gender acc: {:.5f}, avg Mask acc: {:.5f}'.format(epoch, avg_acc, avg_age_acc, avg_gender_acc, avg_mask_acc))
+        print('[epoch: {}] f1_score: {:.5f}, avg train acc: {:.5f}, avg Age acc: {:.5f}, avg Gender acc: {:.5f}, avg Mask acc: {:.5f}'.format(epoch, f1, avg_acc, avg_age_acc, avg_gender_acc, avg_mask_acc))
         
         print("----------------validation!----------------")
         with torch.no_grad():
@@ -133,10 +152,12 @@ def train_models(args):
             mask_model.eval()
             gender_model.eval()
             
+            val_f1 = 0
             total_val_acc = 0
             total_age_acc = 0
             total_mask_acc = 0
             total_gender_acc = 0
+
             for val_iter, val_batch in enumerate(val_dataloader):
                 img, label, gender, age, mask = val_batch
                 img = img.to(device)
@@ -148,26 +169,47 @@ def train_models(args):
                 age_pred = age_model(img)
                 mask_pred = mask_model(img)
                 gender_pred = gender_model(img)
+                pred = torch.max(age_pred, 1)[1] + 3*torch.max(gender_pred, 1)[1] + 6*torch.max(mask_pred, 1)[1]
                 
-                acc_out = torch.sum(torch.max(y_pred, 1)[1] == val_batch_out.data)/len(val_batch_out.data)
-                print('[epoch: {}, iter: {}] validation acc: {}, validation loss: {}'.format(epoch, val_iter, acc_out, loss_out))
-                total_val_loss += loss_out
-                total_val_acc += acc_out
-            avg_val_loss = total_val_loss/len(val_dataloader)
+                val_f1 += f1_score(label.cpu().numpy(), pred.cpu().numpy(), average='macro')
+                age_acc = torch.sum(torch.max(age_pred, 1)[1] == age.data)/len(age.data)
+                gender_acc = torch.sum(torch.max(gender_pred, 1)[1] == gender.data)/len(gender.data)
+                mask_acc = torch.sum(torch.max(mask_pred, 1)[1] == mask.data)/len(mask.data)
+                acc = torch.sum(pred == label)/len(label)
+                
+                print('[epoch: {}, iter: {}] Age validation acc: {:.5f}, Gender validation acc: {:.5f}, Mask validation acc: {:.5f}, Validation acc: {:.5f}'.format(epoch, val_iter, age_acc, gender_acc, mask_acc, acc))
+                
+                total_val_acc += acc
+                total_age_acc += age_acc
+                total_mask_acc += mask_acc
+                total_gender_acc += gender_acc
+            
+            val_f1 = val_f1/(val_iter+1)
             avg_val_acc = total_val_acc/len(val_dataloader)
+            avg_age_acc = total_age_acc/len(val_dataloader)
+            avg_mask_acc = total_mask_acc/len(val_dataloader)
+            avg_gender_acc = total_gender_acc/len(val_dataloader)
         
         # Print
-        print ("epoch:[%d] val_acc: [%.3f], val_loss:[%.3f]."%(epoch, avg_val_acc, avg_val_loss))
+        print('[epoch: {}] f1_score: {:.5f}, avg validation acc: {:.5f}, avg Age acc: {:.5f}, avg Gender acc: {:.5f}, avg Mask acc: {:.5f}'.format(epoch, val_f1, avg_val_acc, avg_age_acc, avg_gender_acc, avg_mask_acc))
         
-        if avg_val_acc > max_val_acc:
-            torch.save(model, '/opt/ml/checkpoint/{}_{}_{}_{:.5f}.pt'.format(now, args.model_name, epoch, avg_val_acc))
-            max_val_acc = avg_val_acc
+        if val_f1 > max_val_f1:
+            torch.save({
+                    'age_model_state_dict': age_model.state_dict(),
+                    'mask_model_state_dict': mask_model.state_dict(),
+                    'gender_model_state_dict': gender_model.state_dict(),
+                    'age_optimizer_state_dict': age_optimizer.state_dict(),
+                    'mask_optimizer_state_dict': mask_optimizer.state_dict(),
+                    'gender_optimizer_state_dict': gender_optimizer.state_dict()
+                    }, '/opt/ml/checkpoint/{}_{}_{}_{:.5f}.pt'.format(now, args.model_name, epoch, val_f1))
+            max_val_f1 = val_f1
             print('----------------model saved!----------------')
     
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--pretrained', default=None, type=str)
+    parser.add_argument('--torchvision', default=None, type=str)
     parser.add_argument('--train_csv', default='/opt/ml/input/data/train/final_train.csv', type=str)
     parser.add_argument('--model_name', default='resnet', type=str)
     parser.add_argument('--epochs', type=int, default=50, help='number of training epochs')
