5ff938d26b160a608b8a5bcbfc05a1e7013e5f30
Dain Kim <77524474+danny980521@users.noreply.github.com>
2021-08-26 09:50:13 +0900

    Add files via upload
    랜덤시드 설정 코드. 서버 중지 후 재개시 결과 달라지는 문제 발생.

---
+++ set_seed.py
@@ -0,0 +1,12 @@
+import random
+
+def seed_everything(seed):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
+    
+seed_everything(42)
