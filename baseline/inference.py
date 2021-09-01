efe40465d640ca0afb62cfa4d6a90780a0428761
pkpete <pkpete@naver.com>
2021-09-01 13:38:45 +0900

    baseline

---
+++ baseline/inference.py
@@ -0,0 +1,100 @@
+import argparse
+import os
+from importlib import import_module
+
+import pandas as pd
+import torch
+from torch.utils.data import DataLoader
+from efficientnet_pytorch import EfficientNet
+
+from dataset import TestDataset, MaskBaseDataset
+
+# 학습 완료된 모델을 통해 test set에 대한 예측 값을 구하고 이를 .csv 형식으로 저장
+
+def load_model(saved_model, num_classes, device):
+    model = None
+    if args.model == 'EfficientNet':
+        model = EfficientNet.from_pretrained('efficientnet-b0', num_classes=18)
+        model.to(device)
+    else:
+        model_module = getattr(import_module("model"), args.model)  # default: BaseModel
+        model = model_module(
+            num_classes=num_classes
+        ).to(device)
+#     model_cls = getattr(import_module("model"), args.model)
+#     model = model_cls(
+#         num_classes=num_classes
+#     )
+
+    # tarpath = os.path.join(saved_model, 'best.tar.gz')
+    # tar = tarfile.open(tarpath, 'r:gz')
+    # tar.extractall(path=saved_model)
+
+    model_path = os.path.join(saved_model, 'best.pth')
+    model.load_state_dict(torch.load(model_path, map_location=device))
+
+    return model
+
+
+@torch.no_grad()
+def inference(data_dir, model_dir, output_dir, args):
+    """
+    """
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    num_classes = MaskBaseDataset.num_classes  # 18
+    model = load_model(model_dir, num_classes, device).to(device)
+    model.eval()
+
+    img_root = os.path.join(data_dir, 'images')
+    info_path = os.path.join(data_dir, 'info.csv')
+    info = pd.read_csv(info_path)
+
+    img_paths = [os.path.join(img_root, img_id) for img_id in info.ImageID]
+    dataset = TestDataset(img_paths, args.resize)
+    loader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=args.batch_size,
+        num_workers=4,
+        shuffle=False,
+        pin_memory=use_cuda,
+        drop_last=False,
+    )
+
+    print("Calculating inference results..")
+    preds = []
+    with torch.no_grad():
+        for idx, images in enumerate(loader):
+            images = images.to(device)
+            pred = model(images)
+            pred = pred.argmax(dim=-1)
+            preds.extend(pred.cpu().numpy())
+
+    info['ans'] = preds
+    info.to_csv(os.path.join(output_dir, f'output.csv'), index=False)
+    print(f'Inference Done!')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    # Data and model checkpoints directories
+    parser.add_argument('--batch_size', type=int, default=64, help='input batch size for validing (default: 64)')
+    parser.add_argument('--resize', type=list, default=[512, 384], help='resize size for image when you trained (default: [512, 384])')
+    parser.add_argument('--model', type=str, default='BaseModel', help='model type (default: BaseModel)')
+
+    # Container environment
+    parser.add_argument('--data_dir', type=str, default=os.environ.get('SM_CHANNEL_EVAL', '/opt/ml/input/data/eval'))
+    parser.add_argument('--model_dir', type=str, default=os.environ.get('SM_CHANNEL_MODEL', '/opt/ml/code/baseline/model/Eff_face_customaug'))
+    parser.add_argument('--output_dir', type=str, default=os.environ.get('SM_OUTPUT_DATA_DIR', './output'))
+
+    args = parser.parse_args()
+
+    data_dir = args.data_dir
+    model_dir = args.model_dir
+    output_dir = args.output_dir
+
+    os.makedirs(output_dir, exist_ok=True)
+
+    inference(data_dir, model_dir, output_dir, args)
